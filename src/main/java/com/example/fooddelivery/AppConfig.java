5e4f59fd0c7d8bc3b596a0c2d7fa3029e423663e
Catalin Crisan <crisan.gheorghecatalin@gmail.com>
2019-05-26 16:46:40 +0300

    Add images resources

--- src/main/java/com/example/fooddelivery/AppConfig.java
+++ src/main/java/com/example/fooddelivery/AppConfig.java
@@ -3,9 +3,17 @@ package com.example.fooddelivery;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
