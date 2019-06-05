cc04fcda572901fab7fd2c0e9129e6a3edc5020c
Catalin Crisan <crisan.gheorghecatalin@gmail.com>
2019-06-05 22:50:45 +0300

    T-6: Create error page

--- src/main/java/com/example/fooddelivery/constants/Views.java
+++ src/main/java/com/example/fooddelivery/constants/Views.java
@@ -2,6 +2,7 @@ package com.example.fooddelivery.constants;
 
 public class Views {
     public static final String MAIN_PAGE = "index";
+    public static final String ERROR_PAGE = "error";
     public static final String CREATE_ORDER_PAGE = "create_order";
     public static final String ORDER_PLACED_PAGE = "order_placed";
     public static final String ORDERS_LIST_PAGE = "orders_list";
